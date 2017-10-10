3ff74ebc4e22c4dbcd16232170a4795cc6ebf697
Aaron Nonaka <31535687+nonakaa@users.noreply.github.com>
2017-10-10 11:10:02 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+
+PlayersList = new Mongo.Collection('players');
+
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function";
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+
+  });
+
+  Template.leaderboard.events({
+'click .player': function(){
+  var playerId = this._id;
+  //console.log("You clicked a .player element");
+  Session.set('selectedPlayer', playerId);
+  var selectedPlayer = Session.get('selectedPlayer');
+  console.log(selectedPlayer);
+}
+  });
+}
+if(Meteor.isServer){
+
+}
